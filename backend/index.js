dd08a2a6f9e8171d385f11e75146c0493dfeb7c5
Febrian Chrisna <febriankrisna77@gmail.com>
2025-05-09 00:36:05 +0700

    tambah cors

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(cors({
   // Allow requests from these origins (add your frontend URL)
   origin: [
     'http://localhost:3000',
+    'https://frontend-notes-febrian-dot-g-09-450802.uc.r.appspot.com/',
     // Add any other frontend origins as needed
   ],
   // Allow credentials (cookies)
