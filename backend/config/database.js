1bd1e22f24c77ad8b9842ca29f67f932d5f07c3b
Febrian Chrisna <febriankrisna77@gmail.com>
2025-05-08 23:40:14 +0700

    cek

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,7 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const db = new Sequelize("notes_051", "root", "febrian123", {
+    host: "34.44.61.23",
     dialect: "mysql",
 });
 
